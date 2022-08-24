3b40232710dc317c5a6b754479e4ecacc9160a0d
Bartosz Ratajczyk <Bartosz.Ratajczyk@itechart-group.com>
2022-08-24 10:10:43 +0200

    register action added

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,8 +1,31 @@
 import { setUser, removeUser } from "../slices/userSlice";
 
-export const logIn = ({ name, surname, email, password }) => {
+export const logIn = ({ email, password }) => {
   return async (dispatch) => {
-    const userId = await registerFirebase({ name, surname, email, password });
+    const userId = await loginAttempt({ email, password });
+
+    const { uID, bio, type, date_of_register, posts } = await getUserFirebase(
+      userId
+    );
+
+    dispatch(
+      setUser({
+        uID,
+        name,
+        surname,
+        email,
+        bio,
+        type: +type,
+        date_of_register,
+        posts,
+      })
+    );
+  };
+};
+
+export const register = ({ name, surname, password, email }) => {
+  return async (dispatch) => {
+    const userId = await registerAttempt({ name, surname, password, email });
 
     const { uID, bio, type, date_of_register, posts } = await getUserFirebase(
       userId

7a51f8d86cd93b62e37ef05ddcc41a743be52e6a
Maikomi <maja.ochmanska@gmail.com>
2022-08-25 15:30:09 +0200

    basic theme

--- src/context/Theme.styled.js
+++ src/context/Theme.styled.js
@@ -2,15 +2,42 @@ import { ThemeProvider } from "styled-components";
 
 const theme = {
   colors: {
-    mono100: "#FFFFFF",
-    mono200: "#F8F8F8",
-    mono300: "#E8E8E8",
-    mono400: "#717171",
-    mono500: "#000000",
+    primary: "#FFFFFF",
+    secondary: "#F8F8F8",
+    tertiary: "#E8E8E8",
+    fontPrim: "#000000",
+    fontSec: "#717171",
+    iconPrim : "#1C1B1F"
   },
   shadows: {
     shadowLight: "2px 2px 10px rgba(0, 0, 0, 0.12);",
   },
+  borderRadius: {
+    xs: "15px",
+    s: '20px',
+    m: "25px",
+    l: '40px',
+  },
+
+  border: 'solid 5px #E8E8E8',
+
+  borderBottom: 'solid 2px #E8E8E8',
+
+  font: {
+    families: {
+      primary: `'Poppins', sans-serif`,
+      icon: 'Material Icons',
+    },
+    sizes: {
+      xxs: '.75px',
+      xs: '.875x',
+      s: '1.1rem',
+      m: '1.3rem',
+      l: '1.5rem',
+      xl: '2rem',
+    },
+  },
+
 };
 
 const Theme = ({ children }) => {
